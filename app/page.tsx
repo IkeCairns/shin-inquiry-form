69f2dcb8fff1b84fa24a9b5c3e4feb37c2c5015d
ikuya <zxh03501@nifty.com>
2024-10-06 16:58:10 +0900

    installed zod and react-hook-form

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+"use client"
+
+
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1>TEST</h1>
     </main>
   );
 }
